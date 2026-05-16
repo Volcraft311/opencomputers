81dc4bf811294168d7c52d739cb6b257c4fe005d
Vladimirs <vladimirskeletons@gmail.com>
2026-05-16 23:26:30 +0300

    dsa

--- update.lua
+++ update.lua
@@ -1,6 +1,6 @@
-if fs.exists(args[1] .. ".lua") then
-    fs.delete(args[1] .. ".lua")
+if fs.exists(arg[1] .. ".lua") then
+    fs.delete(arg[1] .. ".lua")
 end
-print("Downloading " .. args[1] .. ".lua...")
-shell.run("wget https://raw.githubusercontent.com/Volcraft311/opencomputers/main/" .. args[1] .. " " .. args[1])
-print(args[1] .. "Updated successfully!")
+print("Downloading " .. arg[1] .. ".lua...")
+shell.run("wget https://raw.githubusercontent.com/Volcraft311/opencomputers/main/" .. arg[1] .. " " .. arg[1])
+print(arg[1] .. "Updated successfully!")
