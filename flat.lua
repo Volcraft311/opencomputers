575546ccc3a443ce738deb3b38ab813e4bf23859
Vladimirs <vladimirskeletons@gmail.com>
2026-05-17 14:18:37 +0300

    flat

--- flat.lua
+++ flat.lua
@@ -1,5 +1,5 @@
-local forward = arg[1]
-local right = arg[2]
+local forward = tonumber(arg[1])
+local right = tonumber(arg[2])
 
 local function fill_below()
     local depth = 0
@@ -7,10 +7,10 @@ local function fill_below()
        turtle.down()
        depth =  depth + 1
     end
-
+    turtle.digDown()
     for i=1, depth, 1 do
-        turtle.up()
         turtle.placeDown("dirt")
+        turtle.up()
     end
 end
 
@@ -22,13 +22,11 @@ local function move_right()
 end
 
 
-fill_below()
-
-
 for x = 1, right, 1 do
     for z=1, forward, 1 do
         fill_below()
         if turtle.detect() then
+            print("Can't move forward!")
             goto skip_forward
         end
         turtle.forward()
