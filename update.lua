46bd44bf0247d21fb47f468fb99af4a7aa614ae7
Vladimirs <vladimirskeletons@gmail.com>
2026-05-17 13:56:10 +0300

    update

--- update.lua
+++ update.lua
@@ -1,6 +1,10 @@
 if fs.exists(arg[1] .. ".lua") then
     fs.delete(arg[1] .. ".lua")
 end
+
+math.randomseed(os.time())
+local cache_buster = math.random(1, 100000)
+
 print("Downloading " .. arg[1] .. ".lua...")
-shell.run("wget https://raw.githubusercontent.com/Volcraft311/opencomputers/main/" .. arg[1] .. " " .. arg[1])
+shell.run("wget https://raw.githubusercontent.com/Volcraft311/opencomputers/main/" .. arg[1] .. "?nocache=" .. cache_buster .. arg[1] .. ".lua")
 print(arg[1] .. "Updated successfully!")
